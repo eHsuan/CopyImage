1b685a3b5764ca64a9c9422be389b9d810520efd
eHsuan <ac131151@gmail.com>
2025-08-21 18:38:24 +0800

    加入專案檔案。

---
+++ CopyImage/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace CopyImage
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var cts = new CancellationTokenSource();
+
+            var path = Environment.GetEnvironmentVariable("DATA_PATH");
+            //var path = @"D:\test";
+            Console.WriteLine("App started...");
+            while (!cts.Token.IsCancellationRequested) 
+            {
+                
+                if (path != null)
+                {
+                    string target = Path.Join(AppContext.BaseDirectory, "Image");
+                    if (!Directory.Exists(target))
+                    {
+                        Directory.CreateDirectory(target);
+                    }
+                    string[] files = Directory.GetFiles(path);
+                    if (files.Count() > 0)
+                    {
+                        foreach (string file in files)
+                        {
+                            try
+                            {
+                                string fileName = Path.GetFileName(file);
+                                string targetFile = Path.Combine(target, fileName);
+                                File.Copy(file, targetFile);
+                            }
+                            catch
+                            {
+
+                            }
+                        }
+                    }
+                }
+                await Task.Delay(5000); // 模擬每5秒檢查一次
+            }
+            Console.WriteLine("App stopped.");
+
+        }
+    }
+}
